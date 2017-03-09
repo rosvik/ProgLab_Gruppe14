a5a0a3c86da77d01e36f6465128842a9d849444a
Smebbs <mortensmeb@gmail.com>
2017-03-09 17:14:43 +0100

    Add main_robot.ino file.

---
+++ main_robot.ino/main_robot.ino.ino
@@ -0,0 +1,15 @@
+#include <ZumoMotors.h>
+#include <Pushbutton.h>
+
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON);
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
