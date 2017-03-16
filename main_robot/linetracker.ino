541b92892933ba8ef9110dad342775d87be6dee4
Smebbs <mortensmeb@gmail.com>
2017-03-16 17:10:33 +0100

    Move the rest of the code.

--- main_robot/linetracker.ino
+++ main_robot/linetracker.ino
@@ -1,3 +1,19 @@
+#define LED 13
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD 300 //
+
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED 400 // 0 is stopped, 400 is full speed
+#define TURN_SPEED 400
+#define FORWARD_SPEED 400
+#define REVERSE_DURATION 200 // ms
+#define TURN_DURATION 300 // ms
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ZumoReflectanceSensorArray sensors;
+
 void lineTrackerLoop() {
    sensors.read(sensor_values);
   if (sensor_values[0] < QTR_THRESHOLD)
--- main_robot/main_robot.ino
+++ main_robot/main_robot.ino
@@ -2,21 +2,6 @@
 #include <Pushbutton.h>
 #include <QTRSensors.h>
 #include <ZumoReflectanceSensorArray.h>
-#define LED 13
-// this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD 300 //
-
-// these might need to be tuned for different motor types
-#define REVERSE_SPEED 400 // 0 is stopped, 400 is full speed
-#define TURN_SPEED 400
-#define FORWARD_SPEED 400
-#define REVERSE_DURATION 200 // ms
-#define TURN_DURATION 300 // ms
-ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
-#define NUM_SENSORS 6
-unsigned int sensor_values[NUM_SENSORS];
-ZumoReflectanceSensorArray sensors;
 
 void setup()
 {
