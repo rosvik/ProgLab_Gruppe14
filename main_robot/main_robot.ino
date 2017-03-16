3aeb36c1cc76fefad56fee86894d6a18e09ac0d7
Smebbs <mortensmeb@gmail.com>
2017-03-16 16:57:45 +0100

    Linetracker done.

--- main_robot/main_robot.ino
+++ main_robot/main_robot.ino
@@ -1,77 +1,48 @@
-#include <QTRSensors.h>
-#include <ZumoReflectanceSensorArray.h>
 #include <ZumoMotors.h>
 #include <Pushbutton.h>
-ZumoReflectanceSensorArray reflectanceSensors;
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+#define LED 13
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD 300 //
+
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED 400 // 0 is stopped, 400 is full speed
+#define TURN_SPEED 400
+#define FORWARD_SPEED 400
+#define REVERSE_DURATION 200 // ms
+#define TURN_DURATION 300 // ms
 ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON);
-int lastError = 0;
-// This is the maximum speed the motors will be allowed to turn.
-// (400 lets the motors go at top speed; decrease to impose a speed limit)
-const int MAX_SPEED = 400;
-// Set constants for PID control
-const float KP = 0.5; // Proportional constant
-const float KD = 6; //Derivative constant
-const int SV = 2500; // Set-value for position (in the middle of sensors)
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ZumoReflectanceSensorArray sensors;
 
 void setup()
 {
- reflectanceSensors.init();
- button.waitForButton();
- // Turn on LED to indicate we are in calibration mode
- pinMode(13, OUTPUT);
- digitalWrite(13, HIGH);
- // Wait 1 second and then begin automatic sensor calibration
- // by rotating in place to sweep the sensors over the line
- delay(1000);
- int i;
- for(i = 0; i < 80; i++)
- {
- if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
- motors.setSpeeds(-200, 200);
- else
- motors.setSpeeds(200, -200);
-
- reflectanceSensors.calibrate();
- // Since our counter runs to 80, the total delay will be
- // 80*20 = 1600 ms.
- delay(20);
- }
- motors.setSpeeds(0,0);
- // Turn off LED to indicate we are through with calibration
- digitalWrite(13, LOW);
-
- // Wait for the user button to be pressed and released
+ sensors.init();
  button.waitForButton();
 }
 
 void loop()
 {
- unsigned int sensors[6];
- int position = reflectanceSensors.readLine(sensors);
- // Our "error" is how far we are away from the center of the line, which
- // corresponds to position 2500.
- int error = position - SV;
- // do PD computation ( Integral is not used)
- int speedDifference = KP*error + KD * (error - lastError);
-
- lastError = error;
- // Get individual motor speeds. The sign of speedDifference
- // determines if the robot turns left or right.
- int m1Speed = MAX_SPEED + speedDifference;
- int m2Speed = MAX_SPEED - speedDifference;
- // Here we constrain our motor speeds to be between 0 and MAX_SPEED.
- // Generally speaking, one motor will always be turning at MAX_SPEED
- // and the other will be at MAX_SPEED-|speedDifference| if that is positive,
- // else it will be stationary. For some applications, you might want to
- // allow the motor speed to go negative so that it can spin in reverse.
- if (m1Speed < 0)
- m1Speed = 0;
- if (m2Speed < 0)
- m2Speed = 0;
- if (m1Speed > MAX_SPEED)
- m1Speed = MAX_SPEED;
- if (m2Speed > MAX_SPEED)
- m2Speed = MAX_SPEED;
- motors.setSpeeds(m1Speed, m2Speed);
+ sensors.read(sensor_values);
+ if (sensor_values[0] < QTR_THRESHOLD)
+ {
+ // if leftmost sensor detects line, reverse and turn to the right
+ motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+ delay(REVERSE_DURATION);
+ motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+ delay(TURN_DURATION*random(5,15)/10);
+ motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+ }
+ else if (sensor_values[5] < QTR_THRESHOLD)
+ {
+ // if rightmost sensor detects line, reverse and turn to the left
+ motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+ delay(REVERSE_DURATION);
+ motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+ delay(TURN_DURATION);
+ motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+ }
 }
